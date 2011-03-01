505c2ba93c2d0506541f7eae3c418220c4016a83
Will Farrington <will@railsmachine.com>
2011-03-01 16:09:29 -0500

    Expand specs.

--- spec/moonshine/ordered_hash_spec.rb
+++ spec/moonshine/ordered_hash_spec.rb
@@ -1,10 +1,13 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
+class OrderedHashManifest < Moonshine::Manifest::Rails
+end
+
 describe 'ordered hashes' do
   describe 'with ordered hash' do
     it "should be an instance of OrderedHash" do
       h = Hash.new
-      h.class.should == 'ActiveSupport::OrderedHash'
+      h.class.should == ActiveSupport::OrderedHash
     end
 
     it "should be equivalent to an OrderedHash" do
@@ -15,6 +18,20 @@ describe 'ordered hashes' do
         oh[k] = k
       end
       h.should == oh
+    end
+  end
+
+  describe 'with configuration' do
+    before do
+      @manifest = OrderedHashManifest.new
+    end
+
+    it "should always be the same" do
+      h = Hash.new
+      @manifest.configuration.each do |key, value|
+        h[key] = value
+      end
+      h.should == @manifest.configuration
     end
   end
 end
